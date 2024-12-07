a8d62c7b9f80dff93bc2443412e024d057baa1d3
Ehis <iehioze@gmail.com>
2024-12-07 21:01:41 +0000

    initial project commit

---
+++ FeedLoader/FeedLoader/FeedLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoaderApp.swift
+//  FeedLoader
+//
+//  Created by sinbyte on 07/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct FeedLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
